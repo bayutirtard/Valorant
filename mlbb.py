612fc2abec6e0b0643065933817c1b337f2513e0
Bayu Tirta Ardi <92620044+bayutirtard@users.noreply.github.com>
2025-07-15 07:05:18 +0700

    Update mlbb.py

--- mlbb.py
+++ mlbb.py
@@ -4,7 +4,7 @@ from groq import Groq
 # Konfigurasi halaman
 st.set_page_config(page_title="Chatbot MLBB", page_icon="🎮")
 st.title("Chatbot AI Mobile Legends")
-st.image("https://upload.wikimedia.org/wikipedia/en/8/80/Mobile_Legends_Bang_Bang_logo.png", width=150)
+st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh3TK2Xei9vJAv9T86psW5xuy7jlYg6I36kTZidso7eJJIarz-jlILeF2rXld6eA1_wi6LRQmNVWBMbsIxa-jTkyOj7wtWJXMAkCOnXUueoAJNvSk3CqflI-nJiJFj4lPqWslhQ4PwG3N17/s1066/logo-mobile-legend.png", width=150)
 
 # Inisialisasi Groq client
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
