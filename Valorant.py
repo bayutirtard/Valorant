1665a3c2d6306ecad7446575fff411b842e6d7e9
Bayu Tirta Ardi <92620044+bayutirtard@users.noreply.github.com>
2025-08-07 13:29:34 +0700

    Update Valorant.py

--- Valorant.py
+++ Valorant.py
@@ -31,18 +31,21 @@ def load_markdown_data():
 markdown_data = load_markdown_data()
 
 # Inisialisasi riwayat chat jika belum ada
-SYSTEM_PROMPT = (
-    "You are a Valorant expert. You are only allowed to answer based on the data provided below. "
-    "You must not use any outside knowledge. Do not guess. Do not refer to other games. "
-    "If the answer is not clearly found in the data, respond only with: "
-    "\"Sorry, that information is not available in the current database.\" "
-    "Even if the question seems obvious or easy, do not use general knowledge. "
-    "Use ONLY the following data as your source:\n\n"
-    + markdown_data
-)
-
 if "chat_history" not in st.session_state:
-st.session_state.chat_history = [{"role": "system", "content": SYSTEM_PROMPT}]
+    st.session_state.chat_history = [
+        {
+            "role": "system",
+            "content": (
+                "You are a Valorant expert. You are only allowed to answer based on the data provided below. "
+                "You must not use any outside knowledge. Do not guess. Do not refer to other games. "
+                "If the answer is not clearly found in the data, respond only with: "
+                "\"Sorry, that information is not available in the current database.\" "
+                "Even if the question seems obvious or easy, do not use general knowledge. "
+                "Use ONLY the following data as your source:\n\n"
+                + markdown_data
+            )
+        }
+    ]
 
 # Fungsi render chat
 def render_chat(role, content):
@@ -92,8 +95,18 @@ if submit and user_input:
 
 # Tombol reset
 if reset:
-    st.session_state.chat_history = [{"role": "system", "content": SYSTEM_PROMPT}]
+    st.session_state.chat_history = [
+        {
+            "role": "system",
+            "content": (
+                "You are a Valorant expert. You are only allowed to answer based on the data provided below. "
+                "You must not use any outside knowledge. Do not guess. Do not refer to other games. "
+                "If the answer is not clearly found in the data, respond only with: "
+                "\"Sorry, that information is not available in the current database.\" "
+                "Even if the question seems obvious or easy, do not use general knowledge. "
+                "Use ONLY the following data as your source:\n\n"
+                + markdown_data
+            )
+        }
+    ]
     st.rerun()
-
-
-
