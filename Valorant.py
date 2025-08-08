5abfbab7e4d0c6d3650ddf429608b93c14dfef28
Bayu Tirta Ardi <92620044+bayutirtard@users.noreply.github.com>
2025-08-08 21:58:40 +0700

    Update Valorant.py

--- Valorant.py
+++ Valorant.py
@@ -49,7 +49,7 @@ def copy_like_dislike_buttons(text, idx):
             font-size:15px;
             box-shadow:0 2px 8px #0002;
             cursor:pointer;">
-            📋 Copy
+            📋
         </button>
         <button id="likeBtn{idx}" style="
             padding:3px 10px;
@@ -73,7 +73,7 @@ def copy_like_dislike_buttons(text, idx):
         </button>
         <span id="copiedMsg{idx}" style="color:#32CD32; margin-left:8px; display:none; font-size:12px;">Copied!</span>
         <span id="likedMsg{idx}" style="color:#32CD32; margin-left:7px; display:none; font-size:12px;">Thanks for the rating!</span>
-        <span id="dislikedMsg{idx}" style="color:#e04a3c; margin-left:7px; display:none; font-size:12px;">hanks for the feedback!</span>
+        <span id="dislikedMsg{idx}" style="color:#e04a3c; margin-left:7px; display:none; font-size:12px;">Thanks for the feedback!</span>
     </div>
     <script>
     const btnCopy = document.getElementById('copyBtn{idx}');
@@ -163,4 +163,5 @@ for idx, msg in enumerate(st.session_state.chat_history[1:]):  # skip system pro
     if msg["role"] == "assistant":
         copy_like_dislike_buttons(msg.get("raw_markdown", msg["content"]), idx)
     st.markdown("---")
+
 
