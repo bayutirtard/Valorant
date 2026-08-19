d5c1f2ce6c819fc449d0926dbe96ad402a71fbd0
Bayu Tirta Ardi <92620044+bayutirtard@users.noreply.github.com>
2026-08-19 15:51:39 +0700

    Update Valorant.py

--- Valorant.py
+++ Valorant.py
@@ -243,7 +243,7 @@ if submit and user_input:
         st.session_state.current_chat_index = len(st.session_state.all_chats) - 1
     with st.spinner("Answering..."):
         response = client.chat.completions.create(
-            model="meta-llama/llama-4-scout-17b-16e-instruct",
+            model="openai/gpt-oss-120b",
             messages=st.session_state.chat_history["messages"]
         )
         answer = response.choices[0].message.content
